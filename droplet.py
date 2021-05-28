163ab0d441a4bea1a013e7b3dff279a227da742c
Naoto Hori <hori.naoto@gmail.com>
2021-05-28 16:58:16 +0100

    Add platform options to droplet.py

--- droplet.py
+++ droplet.py
@@ -174,6 +174,11 @@ parser.add_argument('-R','--restart', action='store_true',
 parser.add_argument('-r','--res_file', type=str, default='checkpnt.chk',
                     help='checkpoint file for restart')
 
+parser.add_argument('--platform', type=str, default=None,
+                    help='Platform')
+parser.add_argument('--CUDAdevice', type=str, default=None,
+                    help='CUDA device ID')
+
 parser_init = parser.add_mutually_exclusive_group(required=False)
 parser_init.add_argument('-k','--chkpoint', type=str, help='initial xml state')
 parser_init.add_argument('-i','--initpdb', type=str, help='initial structure CG PDB')
@@ -653,9 +658,22 @@ integrator = omm.LangevinIntegrator(simu.temp, 0.5/unit.picosecond, 50*unit.femt
 #platform = omm.Platform.getPlatformByName('CUDA')
 #properties = {'CudaPrecision': 'mixed'}
 
+if args.platform is not None:
+    platform = omm.Platform.getPlatformByName(args.platform)
+    print("Platform ", args.platform)
+else:
+    platform = None
+
+if args.CUDAdevice is not None:
+    properties = {} 
+    properties["DeviceIndex"] = args.CUDAdevice
+    print("DeviceIndex ", args.CUDAdevice)
+else:
+    properties = None
+
 #simulation = app.Simulation(topology, system, integrator, platform)
-#simulation = app.Simulation(topology, system, integrator, platform, properties)
-simulation = app.Simulation(topology, system, integrator)
+simulation = app.Simulation(topology, system, integrator, platform, properties)
+#simulation = app.Simulation(topology, system, integrator)
 
 if simu.restart == False:
 
